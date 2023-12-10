9dbff16d7247a1e9dc60b02a8a4d2389abd499f0
Lawal Oladotun <oladotunlawal7@gmail.com>
2023-12-10 22:56:34 +0000

    fix leak

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { app } from "./app";
 import { connectDB } from "./config/database";
 const admin = require("firebase-admin");
-const serviceAccount = require("../serviceAccountKey.json");
+const serviceAccount = require("../firebaseServiceAccountKey.json");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
