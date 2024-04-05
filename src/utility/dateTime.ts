bae5522bc431701131390cf7bf47f68f450f2523
Lawal Oladotun <oladotunlawal7@gmail.com>
2024-04-05 22:49:33 +0100

    date time fix

--- src/utility/dateTime.ts
+++ src/utility/dateTime.ts
@@ -1,11 +1,9 @@
 export function calculateExpirationTime(secondsUntilExpiry: number) {
   const currentDate = new Date();
-  const currentUtcTimestamp =
-    currentDate.getTime() + currentDate.getTimezoneOffset() * 60 * 1000;
-  const currentUtcDate = new Date(currentUtcTimestamp);
+  const currentUtcTimestamp = currentDate.getTime();
 
   const expirationUtcTimestamp =
-    currentUtcDate.getTime() + secondsUntilExpiry * 1000;
+    currentUtcTimestamp + secondsUntilExpiry * 1000;
   const expirationUtcDate = new Date(expirationUtcTimestamp);
 
   const formattedExpirationTime = expirationUtcDate.toISOString();
